d1fd28093ab2adb5d0a15649d2e78ab21e86c678
Matt Birch <matt---@hotmail.com>
2023-02-24 16:19:55 -0800

    Added a plot of avg score for region 2 vs all other regions. Created necessary features for simple diff-in-diff. Ran one regression.

--- Diff_in_Diff_01_simple.R
+++ Diff_in_Diff_01_simple.R
@@ -1,6 +1,8 @@
 rm(list = ls())
-stem_data <- read.csv("https://raw.githubusercontent.com/MattBirch42/Causal-Inference-Stuff/main/Fake_DD_stemPolicy_scores.csv")
 
+library(ggplot2)
+
+stem_data <- read.csv("https://raw.githubusercontent.com/MattBirch42/Causal-Inference-Stuff/main/Fake_DD_stemPolicy_scores.csv")
 
 #######################################################################################
 ###### DATA DICTIONARY ################################################################
@@ -20,9 +22,36 @@ stem_data <- read.csv("https://raw.githubusercontent.com/MattBirch42/Causal-Infe
 # Score: Assessment exam score (this is our dependent variable)
 # Varsity: did varsity sports
 # single_parent_hh: Lives with single parent
+stem_data$region = as.factor(stem_data$region)
 
 
-# Setting up region 2 diff in diff variables
-stem_data$r2<-ifelse(stem_data$region==2,1,0)
-stem_data$y2013plus<-ifelse(stem_data$year>=2013,1,0)
-stem_data$r2_2013<-stem_data$r2*stem_data$y2013plus
+# Let's do the region 2 diff in diff only. 
+# We'll save region 5 for another video because it is treated at a different year.
+stem2 = stem_data[stem_data$region != 5,]
+
+
+# Create plot of avg scores in region 2 vs other regions combined.
+stem2$reg2 = ifelse(stem2$region == 2,'Region 2','Other Regions')
+
+avg_scores <- aggregate(score ~ reg2 + year, data = stem2, FUN = mean)
+
+ggplot(avg_scores, aes(x = year, y = score, color = reg2)) +
+  geom_line() +
+  labs(x = "Year", y = "Average Score", color = "Region") +
+  theme_light()+
+  geom_vline(xintercept = 2012, linetype = "dashed")
+
+
+
+###################################################################
+# This is how we set up diff-in-diff. Requires at least 3 variables.
+#     Dummy variable for treatment group
+stem2$r2<-ifelse(stem2$region==2,1,0)
+#     Dummy variable for if treatment is happening
+stem2$y2013plus<-ifelse(stem2$year>=2013,1,0)
+#     Interaction: the treatment group is being treated
+stem2$r2_2013<-stem2$r2*stem2$y2013plus
+###################################################################
+
+
+dd_reg1 <- lm(score ~ r2_2013 + r2 + y2013plus, data = stem2)
